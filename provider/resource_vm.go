045c3758505d5b61e9aeaff914b0d7bd2cf250e3
Celine Martha Sari <gorenglahayam@gmail.com>
2024-08-17 21:28:29 +0700

    vm region location

--- provider/resource_vm.go
+++ provider/resource_vm.go
@@ -29,15 +29,126 @@ func ResourceVm() *schema.Resource {
 		},
 	}
 }
+
+func findPrivateNetwork(c Config, uuid string) (string, error) {
+	apiKey := c.ApiKey
+	baseUrl := c.BaseUrl
+	defaultLocation := c.DefaultLocation
+	version := "/v1"
+	path := "/network/networks"
+	fullUrl := baseUrl + version + path
+	if defaultLocation != "" {
+		fullUrl = baseUrl + version + "/" + defaultLocation + path
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fullUrl, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("apikey", apiKey)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 299 || resp.StatusCode < 200 {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return "", fmt.Errorf(string(bodyBytes))
+	}
+	private_network_uuid := ""
+	var result []interface{}
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	if err := json.Unmarshal(bodyBytes, &result); err != nil {
+		return "", err
+	}
+
+	// Iterate over the networks and extract the VM UUIDs
+out:
+	for _, network := range result {
+		if networkMap, ok := network.(map[string]interface{}); ok {
+			if vmUUIDs, ok := networkMap["vm_uuids"].([]interface{}); ok {
+				for _, vmUUID := range vmUUIDs {
+					if uuidStr, ok := vmUUID.(string); ok {
+						if uuidStr == uuid {
+							private_network_uuid = networkMap["uuid"].(string)
+							break out
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return private_network_uuid, nil
+}
+
+func findFloatIp(c Config, uuid string) (string, error) {
+
+	apiKey := c.ApiKey
+	baseUrl := c.BaseUrl
+	defaultLocation := c.DefaultLocation
+	version := "/v1"
+	path := "/network/ip_addresses"
+	fullUrl := baseUrl + version + path
+	if defaultLocation != "" {
+		fullUrl = baseUrl + version + "/" + defaultLocation + path
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fullUrl, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("apikey", apiKey)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 299 || resp.StatusCode < 200 {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return "", fmt.Errorf(string(bodyBytes))
+	}
+	floatip_address := ""
+	var result []interface{}
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	if err := json.Unmarshal(bodyBytes, &result); err != nil {
+		return "", err
+	}
+
+	// Iterate over the networks and extract the assigned_to
+	for _, network := range result {
+		if networkMap, ok := network.(map[string]interface{}); ok {
+			assigned_to, _ := networkMap["assigned_to"].(string)
+			if assigned_to == uuid {
+				floatip_address = networkMap["address"].(string)
+				break
+			}
+		}
+	}
+
+	return floatip_address, nil
+}
+
+// only accept location from provider config
 func vmState(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
 
 	config := m.(*Config)
 	apiKey := config.ApiKey
 	baseUrl := config.BaseUrl
-	path := "/v1/user-resource/vm"
+	defaultLocation := config.DefaultLocation
+	version := "/v1"
+	path := "/user-resource/vm"
+	generatedUrl := baseUrl + version + path
+	if defaultLocation != "" {
+		generatedUrl = baseUrl + version + "/" + defaultLocation + path
+	}
 	uuid := d.Id()
 
-	fullUrl, err := url.Parse(baseUrl + path)
+	fullUrl, err := url.Parse(generatedUrl)
 	if err != nil {
 		return nil, err
 	}
@@ -59,11 +170,68 @@ func vmState(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, err
 	if resp.StatusCode > 299 || resp.StatusCode < 200 {
 		bodyBytes, _ := io.ReadAll(resp.Body)
 		return nil, fmt.Errorf(string(bodyBytes))
-
 	}
+
+	var result map[string]interface{}
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	if err := json.Unmarshal(bodyBytes, &result); err != nil {
+		return nil, err
+	}
+
+	name, _ := result["name"].(string)
+	billing_account_id, _ := result["billing_account"].(float64)
+	username, _ := result["username"].(string)
+	password := "<Hidden>"
+	os_name, _ := result["os_name"].(string)
+	os_version, _ := result["os_version"].(string)
+	vcpu, _ := result["vcpu"].(float64)
+	ram, _ := result["memory"].(float64)
+	private_network_uuid, err := findPrivateNetwork(*config, uuid)
+	if err != nil {
+		return nil, err
+	}
+	float_ip_address, err := findFloatIp(*config, uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	desired_status, _ := result["status"].(string)
+
+	disks_uuid := ""
+	disk_size := 0
+	// Navigate the map to get the value of storage[0].uuid
+	if storageArray, ok := result["storage"].([]interface{}); ok {
+		if firstStorage, ok := storageArray[0].(map[string]interface{}); ok {
+			if storageUuid, ok := firstStorage["uuid"].(string); ok {
+				disks_uuid = storageUuid
+				foundDisk, _ := firstStorage["size"].(float64)
+				disk_size = int(foundDisk)
+			}
+		}
+	}
+	if disks_uuid == "" {
+		return nil, fmt.Errorf("fail to get generated storage UUID")
+	}
+
+	d.Set("uuid", uuid)
+	d.Set("disks_uuid", disks_uuid)
+	d.Set("location", defaultLocation)
+	d.Set("name", name)
+	d.Set("billing_account_id", billing_account_id)
+	d.Set("username", username)
+	d.Set("password", password)
+	d.Set("os_name", os_name)
+	d.Set("os_version", os_version)
+	d.Set("vcpu", int(vcpu))
+	d.Set("ram", int(ram))
+	d.Set("disks", disk_size)
+	d.Set("private_network_uuid", private_network_uuid)
+	d.Set("float_ip_address", float_ip_address)
+	d.Set("desired_status", desired_status)
 
 	return []*schema.ResourceData{d}, nil
 }
+
 func vmCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 
 	config := m.(*Config)
@@ -210,8 +378,19 @@ func vmUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.D
 	config := m.(*Config)
 	apiKey := config.ApiKey
 	baseUrl := config.BaseUrl
-	path := "/v1/user-resource/vm"
-	fullUrl := baseUrl + path
+	// path := "/v1/user-resource/vm"
+	defaultLocation := config.DefaultLocation
+	path := "/user-resource/vm"
+	version := "/v1"
+	fullUrl := baseUrl + version + path
+	location := d.Get("location").(string)
+	if defaultLocation != "" {
+		fullUrl = baseUrl + version + "/" + defaultLocation + path
+	}
+	if location != "" {
+		fullUrl = baseUrl + version + "/" + location + path
+	}
+	// fullUrl := baseUrl + path
 
 	uuid := d.Id()
 	disks_uuid := d.Get("disks_uuid").(string)
@@ -254,8 +433,15 @@ func vmUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.D
 	}
 
 	if d.HasChange("disks") {
-		path = "/v1/user-resource/vm/storage"
-		fullUrl = baseUrl + path
+		version = "/v1"
+		path = "/user-resource/vm/storage"
+		fullUrl = baseUrl + version + path
+		if defaultLocation != "" {
+			fullUrl = baseUrl + version + "/" + defaultLocation + path
+		}
+		if location != "" {
+			fullUrl = baseUrl + version + "/" + location + path
+		}
 		client := &http.Client{}
 		form := url.Values{}
 		form.Add("uuid", uuid)
@@ -288,8 +474,15 @@ func vmUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.D
 
 		if oldAddr != "" {
 			// unassign old ip
-			path = fmt.Sprintf("/v1/network/ip_addresses/%s/unassign", oldAddr)
-			fullUrl = baseUrl + path
+			version = "/v1"
+			path = fmt.Sprintf("/network/ip_addresses/%s/unassign", oldAddr)
+			fullUrl = baseUrl + version + path
+			if defaultLocation != "" {
+				fullUrl = baseUrl + version + "/" + defaultLocation + path
+			}
+			if location != "" {
+				fullUrl = baseUrl + version + "/" + location + path
+			}
 			client := &http.Client{}
 			req, err := http.NewRequest("POST", fullUrl, nil)
 			if err != nil {
@@ -319,8 +512,15 @@ func vmUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.D
 			if err != nil {
 				return diag.FromErr(err)
 			}
-			path = fmt.Sprintf("/v1/network/ip_addresses/%s/assign", newAddr)
-			fullUrl = baseUrl + path
+			version = "/v1"
+			path = fmt.Sprintf("/network/ip_addresses/%s/assign", newAddr)
+			fullUrl = baseUrl + version + path
+			if defaultLocation != "" {
+				fullUrl = baseUrl + version + "/" + defaultLocation + path
+			}
+			if location != "" {
+				fullUrl = baseUrl + version + "/" + location + path
+			}
 			client := &http.Client{}
 			req, err := http.NewRequest("POST", fullUrl, bytes.NewBuffer(jsonData))
 			if err != nil {
